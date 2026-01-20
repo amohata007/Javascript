be752d2d1b0f8d194cca1aa556702c14a93eabc9
Abhishek Mohata <abhishek.mohata@dataforall.org>
2026-01-20 08:01:20 +0530

    Depth Flatten

--- FlattenArray.js
+++ FlattenArray.js
@@ -1,18 +1,16 @@
 let arr = [1,2,[2,3,[1,2,[1]],4],5,[6,5]];
-
+//Using BuildIn Method
 // console.log(arr.flat(Infinity))
 
-let res = [];
 
-function flatten(arr){
-    for(let i of arr){
-        if(Array.isArray(i)){
-            flatten(i);
-        }
-        else{
+function flatten(arr, depth, res = []) {
+    for (let i of arr) {
+        if (Array.isArray(i) && depth > 0) {
+            flatten(i, depth - 1, res);
+        } else {
             res.push(i);
         }
     }
     return res;
 }
-console.log(flatten(arr));
+console.log(flatten(arr, 2));
