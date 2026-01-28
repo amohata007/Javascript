0c1d410ee16411f369260a06d11c8297839405f3
Abhishek Mohata <amohata.007@gmail.com>
2026-01-28 08:21:40 +0530

    2665 - Counter II

---
+++ LC Javascript/2665-COunter2.js
@@ -0,0 +1,26 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let initial = init;
+    return {
+        increment() {
+            return ++init;
+        },
+        decrement() {
+            return --init;
+        },
+        reset(){
+            init = initial;
+            return initial;
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
